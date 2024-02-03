dc48c29a54ddedb9920cd67c41480278479f655e
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-03 12:10:51 +0530

    Getting values from context

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import UserContext from '../context/UserContext'
+import { useContext } from 'react'
 
 const Profile = () => {
-    return (
-        <div>Profile</div>
-    )
+    const { user } = useContext(UserContext);
+
+    if (!user) return <h2>Please login</h2>
+
+    return <h2>Welcome {user.username}</h2>
 }
 
 export default Profile
