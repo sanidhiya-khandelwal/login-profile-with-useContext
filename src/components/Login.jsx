ef0326176e04b83c06601e320a3f1e08489b23fa
Sanidhya Khandelwal <sanidhiyakhandelwal450@gmail.com>
2024-02-03 12:06:23 +0530

    setting up values in userContext

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import UserContext from '../context/UserContext';
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { setUser } = useContext(UserContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({ username, password })
+    }
+
     return (
-        <div>Login</div>
+        <div>
+            <h2>Login</h2>
+            <input type="text"
+                value={username}
+                placeholder='username'
+                onChange={e => setUsername(e.target.value)}
+            />
+            <input type="text"
+                value={password}
+                placeholder='password'
+                onChange={e => setPassword(e.target.value)}
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
     )
 }
 
